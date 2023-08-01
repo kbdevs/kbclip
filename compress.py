66699338aca8030c9ca998fd4ecd915b19c6bdc9
kb <86767129+kbdevs@users.noreply.github.com>
2023-08-01 12:58:34 -0700

    Update compress.py

--- compress.py
+++ compress.py
@@ -1,6 +1,10 @@
 import os
 import subprocess
 from concurrent.futures import ThreadPoolExecutor
+from pathlib import Path
+
+def get_creation_time(file_path):
+    return os.path.getctime(file_path)
 
 def compress_video(input_file, output_file):
     command = (
@@ -13,13 +17,20 @@ def compress_video(input_file, output_file):
 
 def compress_videos(directory):
     with ThreadPoolExecutor(max_workers=4) as executor:
+        video_files = []
         for root, _, files in os.walk(directory):
             for filename in files:
                 if filename.lower().endswith(".mp4"):
                     input_file = os.path.join(root, filename)
-                    output_file = os.path.join(root, f"compressed_{filename}")
-                    executor.submit(compress_video, input_file, output_file)
+                    video_files.append(input_file)
+
+        # Sort the video files by their creation time in descending order (most recent first)
+        video_files.sort(key=get_creation_time, reverse=True)
+
+        for input_file in video_files:
+            output_file = os.path.join(os.path.dirname(input_file), f"compressed_{os.path.basename(input_file)}")
+            executor.submit(compress_video, input_file, output_file)
 
 if __name__ == "__main__":
-    directory_path = input("What directory?: \n")
+    directory_path = input("What directory?: \n")  # Replace this with the actual directory path
     compress_videos(directory_path)
